d7eb4b0d075d999f045cf2093321d32a88318860
nemasisgtx <asjetstreem@gmail.com>
2022-01-05 23:40:12 +0530

    Part 2: Solution

---
+++ src/main/java/Restaurant.java
@@ -0,0 +1,82 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Restaurant {
+    private String name;
+    private String location;
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    private List<Item> menu = new ArrayList<Item>();
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
+
+    public LocalTime getOpeningTime() { return openingTime; }
+
+    public void setOpeningTime(LocalTime openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    public LocalTime getClosingTime() { return closingTime; }
+
+    public void setClosingTime(LocalTime closingTime) {
+        this.closingTime = closingTime;
+    }
+
+    public boolean isRestaurantOpen() {
+        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+        LocalTime time = LocalTime.now();
+        int closedShop = time.compareTo(closingTime);
+        int openShop = time.compareTo(openingTime);
+        if(closedShop<0&&openShop>=0){
+            return true;
+        }
+        return false;
+    }
+
+    public List<Item> getMenu() {
+        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+        return menu;
+    }
+
+    public Item findItemByName(String itemName){
+        for(Item item: menu) {
+            if(item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name,price);
+        menu.add(newItem);
+    }
+    
+    public  void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName);
+
+        menu.remove(itemToBeRemoved);
+    }
+    public void displayDetails(){
+        System.out.println("Restaurant:"+ name + "\n"
+                +"Location:"+ location + "\n"
+                +"Opening time:"+ openingTime +"\n"
+                +"Closing time:"+ closingTime +"\n"
+                +"Menu:"+"\n"+getMenu());
+
+    }
+    public String getName() {
+        return name;
+    }
+}
+//WRITE NEW FEATURE TO BE IMPLEMENTED HERE
---
+++ src/test/java/RestaurantServiceTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.*;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class RestaurantServiceTest {
+
+    RestaurantService service = new RestaurantService();
+    Restaurant restaurant;
+
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+    public void newly_added_restaurants() {
+        restaurant= service.addRestaurant("Amelie's cafe", "chennai", LocalTime.parse("09:30:00"), LocalTime.parse("21:30:00"));
+        service.addRestaurant("Bombay Cafe", "mumbai", LocalTime.parse("12:30:00"), LocalTime.parse("18:00:00"));
+        restaurant.addToMenu("Death By Chocolate", 450);
+        restaurant.addToMenu("Double Patty Hamburger", 650);
+    }
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>SEARCHING<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void searching_for_existing_restaurant_should_return_expected_restaurant_object() throws restaurantNotFoundException {
+        //WRITE UNIT TEST CASE HERE
+        newly_added_restaurants();
+        assertEquals("Bombay Cafe", service.findRestaurantByName("Bombay Cafe").getName());
+    }
+    //You may watch the video by Muthukumaran on how to write exceptions in Course 3: Testing and Version control: Optional content
+    @Test
+    public void searching_for_non_existing_restaurant_should_throw_exception() throws restaurantNotFoundException {
+        //WRITE UNIT TEST CASE HERE
+        //<<<<<<<<<<<<<<<<<<<<SEARCHING>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    }
+        //>>>>>>>>>>>>>>>>>>>>>>ADMIN: ADDING & REMOVING RESTAURANTS<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        @Test
+        public void remove_restaurant_should_reduce_list_of_restaurants_size_by_1 () throws restaurantNotFoundException
+        {
+            newly_added_restaurants();
+            int initialNumberOfRestaurants = service.getRestaurants().size();
+            service.removeRestaurant("Amelie's cafe");
+            assertEquals(initialNumberOfRestaurants - 1, service.getRestaurants().size());
+        }
+
+        @Test
+        public void removing_restaurant_that_does_not_exist_should_throw_exception () throws restaurantNotFoundException
+        {
+            newly_added_restaurants();
+            assertThrows(restaurantNotFoundException.class, () -> service.removeRestaurant("Pantry d'or"));
+        }
+
+        @Test
+        public void add_restaurant_should_increase_list_of_restaurants_size_by_1 () {
+            newly_added_restaurants();
+            int initialNumberOfRestaurants = service.getRestaurants().size();
+            service.addRestaurant("Pumpkin Tales", "Chennai", LocalTime.parse("12:00:00"), LocalTime.parse("23:00:00"));
+            assertEquals(initialNumberOfRestaurants + 1, service.getRestaurants().size());
+        }
+        //<<<<<<<<<<<<<<<<<<<<ADMIN: ADDING & REMOVING RESTAURANTS>>>>>>>>>>>>>>>>>>>>>>>>>>
+    }
